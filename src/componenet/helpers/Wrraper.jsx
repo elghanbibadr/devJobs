6132952f848c6caa9621842a0eefa71ac5823fd2
badr ghanbi <bghanbi50@gmail.com>
2023-01-09 23:16:40 +0100

    add normalise css

--- src/componenet/helpers/Wrraper.jsx
+++ src/componenet/helpers/Wrraper.jsx
@@ -4,8 +4,8 @@ import Home from '../Home/Home'
 import { AppContext } from '../../store/AppContext'
 
 const Wrraper=()=>{
+  const {isUserSelectJobId , setIsUserSelectJobId}=useContext(AppContext);
 
-const {isUserSelectJobId , setIsUserSelectJobId}=useContext(AppContext);
   return (
      <main className="bg-darkBlue dark:bg-darkWhite">
      <section className=" max-w-8xl p-8 mx-auto ">
